5e2dea3011820479987fcb7f1753436e6a1fc27f
Jakaria <41715205+jakaria9001@users.noreply.github.com>
2021-02-03 18:43:47 +0530

    Create primeGame.cpp

---
+++ Others/primeGame.cpp
@@ -0,0 +1,7 @@
+// Link : https://www.scaler.com/academy/mentee-dashboard/classroom/math-for-quant-2510b433-ae78-4883-91dd-659693c6cf71/#homework
+
+int Solution::solve(int A, int B) {
+    if(A%2==0) return 2;
+    else if(A%2 && B==1) return 2;
+    return 1;
+}
