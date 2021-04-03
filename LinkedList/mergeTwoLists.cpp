4a4591a4232dfe6ab6b6862bd5d851ed1757122d
Jakaria <41715205+jakaria9001@users.noreply.github.com>
2021-04-04 00:51:51 +0530

    Create mergeTwoLists.cpp

---
+++ LinkedList/mergeTwoLists.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ // Merge Two lists : 
+ListNode* Solution::mergeTwoLists(ListNode* A, ListNode* B) {
+    if(!A) return B;
+    if(!B) return A;
+    
+    if(A->val < B->val)
+    {
+        A->next = mergeTwoLists(A->next, B);
+        return A;
+    }
+    else {
+        B->next = mergeTwoLists(A, B->next);
+        return B;
+    }
+    return NULL;
+}
