ced9e0b1cb26a6c5148b4903b1a2ac06ed26559a
Jakaria <41715205+jakaria9001@users.noreply.github.com>
2021-04-20 00:51:22 +0530

    Create maxWidthOfTree.cpp

---
+++ Tree/maxWidthOfTree.cpp
@@ -0,0 +1,23 @@
+// https://practice.geeksforgeeks.org/problems/maximum-width-of-tree/1/?track=DSASP-Tree&batchId=154
+int getMaxWidth(Node* root)
+    {
+        
+       // Your code here
+       if(!root) return 0;
+       
+       int ans = -1;
+       queue<Node*> q;
+       q.push(root);
+       while(!q.empty()){
+           int n = q.size();
+           
+            if(n>=ans) ans = n;
+           while(n--){
+               Node* temp = q.front();
+               q.pop();
+               if(temp->left) q.push(temp->left);
+               if(temp->right) q.push(temp->right);
+           }
+       }
+       return ans;
+    }
