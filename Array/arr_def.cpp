d7b166b9c5746f673fbb11dead38cc8442ee599c
Jakaria <41715205+jakaria9001@users.noreply.github.com>
2021-05-30 23:36:52 +0530

    Create arr_def.cpp

---
+++ Array/arr_def.cpp
@@ -0,0 +1,8 @@
+/*
+An array is a contiguous allocation of blocks of memory of similar datatype
+
+An array can be of inteher, character or any data type.
+
+We can store array of structs as well.
+
+*/
