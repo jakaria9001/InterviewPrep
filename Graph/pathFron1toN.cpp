3a602c31c8eb3f5e3a32420c9511696981dbce09
Hussain <41715205+jakaria9001@users.noreply.github.com>
2021-07-12 00:43:37 +0530

    Update pathFron1toN.cpp

--- Graph/pathFron1toN.cpp
+++ Graph/pathFron1toN.cpp
@@ -1,21 +1,42 @@
 // Link: https://www.interviewbit.com/problems/path-in-directed-graph/
-bool dfs(vector<vector<int>>&adj, vector<bool>&vis, int start, int end){
-    vis[start] = true;
-    if(start==end) return true;
 
-    for(auto i: adj[start]){
-
-            if(!vis[i]){
-                if(dfs(adj, vis,i,end)) return true;
+bool bfs(vector<vector<int>>& adj, int V){
+    if(V==1) return true;
+    vector<bool>visited(V+1, false);
+    queue<int> q;
+    q.push(1);
+    visited[1]=true;
+    while(!q.empty()){
+        int temp = q.front();
+        q.pop();
+        for(auto i: adj[temp]){
+            if(!visited[i]){
+                if(i==V) return true;
+                visited[i] = true;
+                q.push(i);
             }
+        }
     }
     return false;
 }
-int Solution::solve(int A, vector<vector<int> > &B) {
-    vector<vector<int>> adj(A+1);
-    for(int i=0;i<B.size();i++){
-        adj(B[i][0]).push_back(B[i][1]);
+
+bool dfs(vector<vector<int>> &adj, vector<bool> &visited, int start, int dest){
+    visited[start] = true;
+    if(start == dest) return true;
+    for(int i = 0; i<adj[start].size(); i++){
+        if(!visited[adj[start][i]]){
+            if(dfs(adj, visited, adj[start][i], dest)) return true;
+        }
     }
-    vector<int> visited(A+1, false);
-    return dfs(adj, visited, 1, A);
+    return false;
+}
+
+int Solution::solve(int A, vector<vector<int>> &B) {
+    vector<vector<int>> adj(A+1);
+    for(int i = 0; i<B.size(); i++){
+        adj[B[i][0]].push_back(B[i][1]); // directed graph
+    }
+    // vector<bool> visited(A+1, false);
+    // return dfs(adj, visited, 1, A);
+    return bfs(adj, A);
 }
