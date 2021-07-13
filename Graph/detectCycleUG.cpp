f25e1ab5b1f8eb900dc73aa61ef1c75736614000
Hussain <41715205+jakaria9001@users.noreply.github.com>
2021-07-14 00:02:03 +0530

    Update detectCycleUG.cpp

--- Graph/detectCycleUG.cpp
+++ Graph/detectCycleUG.cpp
@@ -34,3 +34,37 @@ int main(){
   return 0;
 }
 
+/*
+
+// Link: https://www.interviewbit.com/problems/cycle-in-undirected-graph/
+bool dfs(vector<vector<int>>& adj, vector<bool>& visited,int src, int parent){
+  visited[src]=true;
+  for(auto i: adj[src]){
+    if(!visited[i]){
+      if(dfs(adj,visited,i,src)) return true;
+    }else if(i!=parent)
+          return true;
+  }
+  return false;
+  
+}
+
+int Solution::solve(int A, vector<vector<int> > &B) {
+    vector<vector<int>> input=B, adj(A+1);
+
+    for(int i=0;i<input.size();i++){
+        adj[input[i][0]].push_back(input[i][1]);
+        adj[input[i][1]].push_back(input[i][0]); 
+    }
+    
+    vector<bool>visited(A+1,false);
+  
+    for(int i=1;i<=A;i++){
+        if(!visited[i]){
+            if(dfs(adj,visited,i,-1)) return true;
+        }
+    }
+    return false;
+}
+*/
+
