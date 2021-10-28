ad06a222a34d5722f9ba0401ed56161267d159c7
Hussain <41715205+jakaria9001@users.noreply.github.com>
2021-10-28 23:09:45 +0530

    Create maxAreaHistogram.cpp

---
+++ Stack/maxAreaHistogram.cpp
@@ -0,0 +1,16 @@
+// Link: https://leetcode.com/problems/largest-rectangle-in-histogram/
+int Solution::largestRectangleArea(vector<int> &height) {
+    stack<int> s;
+		height.push_back(0);
+		int result = 0;
+		for (int i = 0; i < height.size(); ) {
+			if (s.empty() || height[i] > height[s.top()])
+				s.push(i++);
+			else {
+				int tmp = s.top();
+				s.pop();
+				result = max(result, height[tmp] * (s.empty() ? i : i - s.top() - 1));
+			}
+		}
+		return result;
+}
